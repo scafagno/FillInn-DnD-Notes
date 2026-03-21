dae8d9b1c4d4bab7a1a5720c085bb6129ce4d8b8
scafagno <samuele.cafagno@gmail.com>
2026-03-21 23:00:24 +0100

    Quartz sync: Mar 21, 2026, 11:00 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  pageBody: [Component.RecentNotes()]
+  pageBody: [Component.RecentNotes()],
   afterBody: [],
   footer: Component.Footer({
     links: {
